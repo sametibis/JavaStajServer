f02e7b11cd2121784d47afaba6a79832147428ca
sametibis <sametibs.dev@gmail.com>
2020-10-16 18:40:53 +0300

    Spring Security: BCrypt

--- src/main/java/com/hoaxify/ws/user/UserService.java
+++ src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,19 +1,22 @@
 package com.hoaxify.ws.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	
 	IUserRepository userRepository;
+	PasswordEncoder passwordEncoder;
 	
 	public UserService(IUserRepository userRepository) {
-		this.userRepository = userRepository;
-	} // IUserRepository üzeindeki @Autowired ile eşdeğer.
-
-
-
+		this.userRepository = userRepository; // IUserRepository üzeindeki @Autowired ile eşdeğer.
+		this.passwordEncoder = new BCryptPasswordEncoder();
+	} 
+	
 	public void save(User user) {
+		String encryptedPassword = this.passwordEncoder.encode(user.getPassword());
+		user.setPassword(encryptedPassword);
 		userRepository.save(user);
 		
 	}
