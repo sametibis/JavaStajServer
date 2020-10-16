97ec4344009b581d2f3635fe333c0f43aadc81ef
sametibis <sametibs.dev@gmail.com>
2020-10-16 03:14:57 +0300

    Test Commit

--- src/main/java/com/hoaxify/ws/WsApplication.java
+++ src/main/java/com/hoaxify/ws/WsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WsApplication {
 
 	public static void main(String[] args) {
+		// Test Comment
 		SpringApplication.run(WsApplication.class, args);
 	}
 
